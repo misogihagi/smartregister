08efe5f3e048caead1daf6fcc604b5eef9ed60cd
misogihagi <47350059+misogihagi@users.noreply.github.com>
2021-02-03 00:34:20 +0900

    Update beluga.py

--- beluga.py
+++ beluga.py
@@ -1,25 +1,76 @@
-mport requests
+import requests
 import urllib
 import json
+from collections.abc import Iterable
+
+debug = False
+
 
 class Client:
-def __init__(self, smrg_access_key_id, smrg_secret_access_key, contract_id, grant_type='client_credentials', scopes, scope,debug=true):
-self._endpoint = "https://id.smaregi.{}/{}/".format('dev' if debug else 'jp', contract_id)
-url = "https://id.smaregi.{}/app/{}/token".format('dev' if debug else 'jp', contract_id)
-params = urllib.parse.urlencode({
-'grant_type': grant_type,
-'scope': scope or ' '.join(scopes)
-})
-headers = {
-'Content-Type': 'application/x-www-form-urlencoded'
-}
-r = requests.post(url=url, data=params, headers=headers,
-auth=(smrg_access_key_id, smrg_secret_access_key)
+    def __init__(self,
+                 smrg_access_key_id: str, smrg_secret_access_key: str,
+                 contract_id: str, scopes: Iterable,
+                 grant_type='client_credentials', scope='', development=True):
+        if debug == True:
+            url = "{}/app/{}/token".format('http://localhost:8080',
+                                           contract_id)
+            endhost = 'http://localhost:8080'
+        else:
+            url = "{}/app/{}/token".format('https://id.smaregi.{}'.format(
+                'dev' if development else 'jp'), contract_id)
+            endhost = 'https://api.smaregi.{}'.format(
+                'dev' if development else 'jp')
+        self._endpoint = "{}/{}/".format(endhost, contract_id)
+        params = urllib.parse.urlencode({
+            'grant_type': grant_type,
+            'scope': scope or ' '.join(scopes)
+        })
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        r = requests.post(
+            url=url,
+            data=params,
+            headers=headers,
+            auth=(smrg_access_key_id, smrg_secret_access_key)
+        )
 
-self.access_token = json.loads(r.text)['access_token']
+        self.access_token = json.loads(r.text)['access_token']
 
-def list_menu:
+    def list_menu(self):
+        url = self._endpoint + 'waiter/menus'
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+        r = requests.get(
+            url=url,
+            headers=headers,
+        )
+        return json.loads(r.text)
 
-url = self._endpoint + 'waiter/menus'
-req = urllib.request.Request(url)
-req.add_header('Authorization', 'Bearer '+at)
+    def check_in(self, params):
+        url = self._endpoint + 'waiter/table_uses'
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token),
+            'Content-Type': 'application/json'
+        }
+        r = requests.post(
+            url=url,
+            data=json.dumps(params),
+            headers=headers
+        )
+        return json.loads(r.text)
+
+    def order(self, id, params):
+        url = self._endpoint + 'waiter/table_uses/' + id + '/orders'
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.access_token),
+            'Content-Type': 'application/json'
+        }
+        r = requests.post(
+            url=url,
+            data=json.dumps(params),
+            headers=headers
+        )
+        return json.loads(r.text)
+
